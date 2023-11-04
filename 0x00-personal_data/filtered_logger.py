e5f3d3f4b6f828cb3a426506c1a0e191faa8b91e
Dev-wonderful <adeonederful20@gmail.com>
2023-11-04 23:59:53 +0100

    annotation and linting

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Filtered logger module"""
 import re
+from typing import List
 
-def filter_datum(fields: list, redaction: str, message: 
-                 list, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str, message:
+                 str, separator: str) -> str:
     """Filter the log info"""
     for field in fields:
-        pattern = r'{}=([^;]+)'.format(field)
-        replacement = '{}={}'.format(field, redaction)
+        pattern: str = r'{}=([^;]+)'.format(field)
+        replacement: str = '{}={}'.format(field, redaction)
     return re.sub(pattern, replacement, message)
