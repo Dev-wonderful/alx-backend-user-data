9a30d58127d07fd2f6df7560f5d86478144a1190
Dev-wonderful <adeonederful20@gmail.com>
2023-11-05 00:22:06 +0100

    correct output

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,21 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message:
                  str, separator: str) -> str:
-    """Filter the log info"""
+    """Filter the log info
+
+    Args:
+        fields (list): A list of strings representing all fields to obfuscate
+        redaction (str): A string representing by what the field will
+                         be obfuscated
+        message (str): A string representing the log line
+        separator (str): A string representing by which character is
+                         separating all fields in the log line
+
+    Returns:
+        str: The log message obfuscated
+    """
     for field in fields:
         pattern: str = r'{}=([^;]+)'.format(field)
         replacement: str = '{}={}'.format(field, redaction)
-    return re.sub(pattern, replacement, message)
+        message = re.sub(pattern, replacement, message)
+    return message
