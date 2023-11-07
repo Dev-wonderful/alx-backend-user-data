5650f9da0992d2a41b20efd584d97d22d30bb701
Dev-wonderful <adeonderful20@gmail.com>
2023-11-07 17:46:25 +0100

    template auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for the auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """The template Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check for auth path"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """get the auth header"""
+        return request
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Get the current user"""
+        return request
