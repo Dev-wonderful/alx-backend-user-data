fbac3617555c3655459b2f7cb3709b664c52de78
Dev-wonderful <adeonderful20@gmail.com>
2023-11-08 02:45:08 +0100

    auth header screening

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,19 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """The basic auth class implementation"""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ returns the Base64 part of the Authorization header
+            for a Basic Authentication
+        """
+        if (authorization_header is None or
+            not isinstance(authorization_header, str)):  # noqa: E129
+            return None
+        auth_header: list = authorization_header.split()
+        if len(auth_header) != 2:
+            return None
+        type, encoding = auth_header
+        if type != 'Basic':
+            return None
+        return encoding
