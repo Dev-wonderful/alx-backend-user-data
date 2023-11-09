e81fc14abb9a44a2b243769ff2a5d7878b760842
Dev-wonderful <adeonderful20@gmail.com>
2023-11-09 13:21:59 +0100

    get current user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """Module for the Session Authentication"""
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -21,3 +22,13 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """Get the surrent user"""
+        if request is None:
+            return None
+        # get session and user ID
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        if user_id is None:
+            return None
+        return User.get(user_id)
