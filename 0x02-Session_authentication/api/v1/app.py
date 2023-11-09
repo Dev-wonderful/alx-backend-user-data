0dd7b6b15f1b99a4d558b8174338273f147b2ca5
Dev-wonderful <adeonderful20@gmail.com>
2023-11-09 11:03:37 +0100

    excluded login path

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,11 +33,12 @@ def handle_before_request():
     if auth is None:
         return
     excluded_paths = [
-        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth.require_auth(request.path, excluded_paths) is False:
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None or auth.session_cookie(request) is None:  # noqa: E501
         abort(401)
     request.current_user = auth.current_user(request)
     if request.current_user is None:
