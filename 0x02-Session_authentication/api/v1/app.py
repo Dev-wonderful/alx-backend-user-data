cba9de3824a37ce01e20f45237a701379be15145
Dev-wonderful <adeonederful20@gmail.com>
2023-11-11 11:03:51 +0000

    :bug: typing errors

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from typing import Union
 import os
 
 
@@ -16,7 +17,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 # print(os.environ)
-auth: str | Auth | BasicAuth | SessionAuth = None
+auth: Union[str, Auth, BasicAuth, SessionAuth] = None
 auth_config = {
     'auth': Auth(),
     'basic_auth': BasicAuth(),
