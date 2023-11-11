8c5adde43fef39cc02dfdbaae50596dd9de4bc46
Dev-wonderful <adeonderful20@gmail.com>
2023-11-11 11:05:14 +0100

    set abort

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,12 +28,14 @@ if auth_type in auth_config.keys():
 
 
 @app.before_request
-def handle_before_request():
+def run_before_every_request():
     """Check auth before every request"""
     if auth is None:
         return
     excluded_paths = [
-        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
         '/api/v1/auth_session/login/'
     ]
     if auth.require_auth(request.path, excluded_paths) is False:
@@ -54,14 +56,14 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def not_found(error) -> str:
+def unauthorized(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def not_found(error) -> str:
+def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
