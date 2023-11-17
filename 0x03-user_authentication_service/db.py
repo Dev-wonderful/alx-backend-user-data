7f1ce523189444e087eee63d40db7f99936c1c52
Dev-wonderful <adeonderful20@gmail.com>
2023-11-17 02:47:38 +0100

    raise invalid error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-# from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
@@ -45,7 +45,10 @@ class DB:
     def find_user_by(self, *args, **kwargs):
         """find user by certain attributes"""
         session = self.__session
-        result = session.query(User).filter_by(**kwargs).first()
+        try:
+            result = session.query(User).filter_by(**kwargs).first()
+        except InvalidRequestError:
+            raise InvalidRequestError
         # print(result)
         if result is None:
             raise NoResultFound
