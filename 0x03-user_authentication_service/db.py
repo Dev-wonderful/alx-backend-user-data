c1f2060aa47d6940d2d31521057addb36b2dcfcd
Dev-wonderful <adeonderful20@gmail.com>
2023-11-17 02:42:58 +0100

    find user by attribute

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+# from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -39,3 +41,12 @@ class DB:
         session.add(new_user)
         session.commit()
         return new_user
+
+    def find_user_by(self, *args, **kwargs):
+        """find user by certain attributes"""
+        session = self.__session
+        result = session.query(User).filter_by(**kwargs).first()
+        # print(result)
+        if result is None:
+            raise NoResultFound
+        return result
