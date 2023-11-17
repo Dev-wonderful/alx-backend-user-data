22c1558577c8c9011021b82847c8e310f78a1a84
Dev-wonderful <adeonderful20@gmail.com>
2023-11-17 15:27:47 +0100

    value error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,7 +89,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            return None
+            raise ValueError
         reset_token = _generate_uuid()
         user.reset_token = reset_token
         try:
